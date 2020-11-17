8c1018f3c0478e99a581c276e15a85f393ab7169
Felipe <felipebruckmann@hotmail.com.br>
2020-11-17 17:50:36 -0300

    arrumando o textFormat.js

--- utils/textFormat.js
+++ utils/textFormat.js
@@ -1,10 +1,10 @@
 module.exports = (value) => {
   if (value >= 23 && value <= 26) {
-    return `A temperatura de hoje é de: ${value}°, e esta ideal!`;
+    return `A temperatura de hoje é de: ${value}°, e o clima esta ideal!`;
   } 
   else if (value < 23 ) {
-    `A temperatura de hoje é de ${value}°, e esta começando a esfriar, vista seu casaco!`;
+    `A temperatura de hoje é de ${value}°, e o clima esta começando a esfriar, vista seu casaco!`;
   } else {
-    `A temperatura de hoje é de ${value}°, o calor vem aí, se prepare!`
+    `A temperatura de hoje é de ${value}°, o calor vem aí, o clima ira esquentar, se prepare!`
   }
 };
