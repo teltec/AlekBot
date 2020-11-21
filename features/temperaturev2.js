5cc220faf601ab6ce2643ef40028112e59fbaf81
Felipe <felipebruckmann@hotmail.com.br>
2020-11-21 17:47:53 -0300

    - Making some tests on temperature.js

--- features/temperaturev2.js
+++ features/temperaturev2.js
@@ -1,35 +1,35 @@
-const axios = require("axios");
+// const axios = require("axios");
 
-module.exports = async (controller) => {
-  const convo = new BotkitConversation("temperature", controller);
+// module.exports = (controller) => {
+//   const convo = new BotkitConversation("temperature", controller);
 
-  convo.ask(
-    "De qual cidade você deseja saber a temperatura?",
-    async (answer, convo, bot) => {
-      const city = answer;
-      const config = {
-        method: "get",
-        url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
-          ${apikey}units=metric`,
-        headers: {},
-      };
-      try {
-        await axios(config).then(function (response) {
-          const temperature = parseFloat(response.data.main.temp);
-          const text = `A temperatura de hoje em ${city} é de: ${temperature}`;
-          convo.say(text);
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  );
+//   convo.ask(
+//     "De qual cidade você deseja saber a temperatura?",
+//     async (answer, convo, bot) => {
+//       const city = answer;
+//       const config = {
+//         method: "get",
+//         url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
+//           ${apikey}units=metric`,
+//         headers: {},
+//       };
+//       try {
+//         await axios(config).then(function (response) {
+//           const temperature = parseFloat(response.data.main.temp);
+//           const text = `A temperatura de hoje em ${city} é de: ${temperature}`;
+//           convo.say(text);
+//         });
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     }
+//   );
 
-  controller.hears(
-    "temperature",
-    "message,direct_message",
-    async (bot, message) => {
-      await bot.beginDialog("temperature");
-    }
-  );
-};
+//   controller.hears(
+//     "temperature",
+//     "message,direct_message",
+//     async (bot, message) => {
+//       await bot.beginDialog("temperature");
+//     }
+//   );
+// };
