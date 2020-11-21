f1beae4b30b82b3c9987b3eeb8e113bef6b089ab
Felipe <felipebruckmann@hotmail.com.br>
2020-11-21 17:39:25 -0300

    - Fixing some issues on temperature.js

--- features/temperaturev2.js
+++ features/temperaturev2.js
@@ -1,38 +1,35 @@
-const axios = require('axios');
-
+const axios = require("axios");
 
 module.exports = async (controller) => {
-
-  const convo = new BotkitConversation( 'temperature', controller );
-  let city = '';
-  const config = {
-    method: 'get',
-    url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
-    ${apikey}units=metric`,
-    headers: { }
-  };
+  const convo = new BotkitConversation("temperature", controller);
 
   convo.ask(
-      'De qual cidade você deseja saber a temperatura?',
-      async ( answer, convo, bot ) => {
-        city = answer;
-      },
+    "De qual cidade você deseja saber a temperatura?",
+    async (answer, convo, bot) => {
+      const city = answer;
+      const config = {
+        method: "get",
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
+          ${apikey}units=metric`,
+        headers: {},
+      };
+      await axios(config)
+        .then(function (response) {
+          const temperature = parseFloat(response.data.main.temp);
+          const text = `A temperatura de hoje em ${city} é de: ${temperature}`;
+          convo.say(text);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
   );
 
-  convo.say(async(convo, bot) => {
-    
-  await axios(config).then(function (response) {
-    const temperature = parseFloat(response.data.main.temp);
-    const text = `A temperatura de hoje em ${city} é de: ${temperature}`;
-
-  }).catch(function (error) {
-    console.log(error);
-  });
-  })
-
-  controller.hears( 'temperature', 'message,direct_message', async ( bot, message ) => {
-    await bot.beginDialog( 'temperature' );
-});
-  
-}
-
+  controller.hears(
+    "temperature",
+    "message,direct_message",
+    async (bot, message) => {
+      await bot.beginDialog("temperature");
+    }
+  );
+};
